3de62da33b8cd0e8ed3cc1659fdb4bc3229378e1
Ramadhan <miqra1612@gmail.com>
2025-06-19 17:24:42 +0700

    Fix spacing

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = memo(() => {
   return (
     <>
         <div className="justify-center">
-            <h6 className="bg-gradient-to-r from-orange-600 to-amber-400 bg-clip-text text-transparent text-md max-sm:text-sm font-bold  py-2  mt-1"> 2025 All Right Reserve || Website by M.I.Ramadhan</h6>
+            <h6 className="bg-gradient-to-r from-orange-600 to-amber-400 bg-clip-text text-transparent text-md max-sm:text-sm font-bold  py-2  mt-10"> 2025 All Right Reserve || Website by M.I.Ramadhan</h6>
             
         </div>
     </>
